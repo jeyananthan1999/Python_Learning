f502127d86c75a73ef0a9806fc9e8f13dc04838d
Jeyananthan S <jeyananthan1999@gmail.com>
2026-05-26 11:18:09 +0530

    Add Variables.py for variable demonstration

---
+++ Introduction/Variables.py
@@ -0,0 +1,17 @@
+name = "Jey"
+language = "Python"
+print("My name is",name)
+print(name,"is learning",language)
+print(name,"wants to become",language,"expert")
+
+#Variable Challenge
+
+user = "datawithbaraa"
+domain = ".com"
+print("info@",user,domain)
+print("support@",user,domain)
+print("www.",user,domain)
+
+print("info@" + user + domain)
+print("support@" + user + domain)
+print("www." + user + domain)

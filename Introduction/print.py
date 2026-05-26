6946f74433ebf78ea7344d058ed690168a941503
Jeyananthan S <jeyananthan1999@gmail.com>
2026-05-26 10:47:28 +0530

    Add escape sequences and formatting examples to print.py

--- Introduction/print.py
+++ Introduction/print.py
@@ -1,2 +1,35 @@
 print("Hi Python") #Double Quotes
 print('Hello Python') #Single Quotes
+
+#Escape Sequences
+print("Hi \"Python\"")            #--> OUTPUT: Hi "Python"
+print('Hi "Python"')              #--> OUTPUT: Hi "Python"
+print("Hi \'Python\'")            #--> OUTPUT: Hi 'Python'
+print("Hi 'Python'")              #--> OUTPUT: Hi 'Python'
+
+#------ ESCAPE BACKSLASH ----------#
+print("Path: C:\\Users\\Jey")               #OUTPUT: Path: C:\Users\Jey
+
+#------ ESCAPE NEW LINE -----------#
+print("MESSAGE1\n")
+print("MESSAGE2")
+
+print("MESSAGE1\nMESSAGE2")
+
+print("MESSAGE1\n\n\n")   #-- 3 NEW LINES
+print("MESSAGE2")
+
+print("MESSAGE1\n\n\nMESSAGE2") #-- 2 NEW LINES
+
+#------ ESCAPE TAB -----------#
+print("Message1 Message2")
+print("Message1\tMessage2") #-- Adds a Tab space 
+
+#PYTHON PRINT CHALLENGE
+print("Your Learning Path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+
+#Triple Quotes
+print("""Your Learning Path:
+\t- Python Basics
+\t- Data Engineering
+\t- AI""")                  #-- Easily Readble and Neat Formatting
